1e29bbd2df424f415e850e9ac8f48d4e56ce4765
Geetanjali <85665491+geetanjali1011@users.noreply.github.com>
2023-08-20 08:53:16 +0530

    Add files via upload

---
+++ Day1 - 8.7.23/Day1.py
@@ -0,0 +1,14 @@
+
+
+print('Purpose of the Program is to take your data...')
+
+name = input("Mention your Name : \n")
+print("\nName given ==> ", name)
+print() # end = "\n"
+
+age = input("Write your Age : ")
+print("Given AGE is,", age)
+
+
+salary = input("MAke sure you write Salary in $format : ")
+print("SALARY", salary)
